751d85004fc4a87a6d6504d1580a329f7f980904
Keshav <keshavrokaya1244@gmail.com>
2025-03-06 12:34:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
                           </div>
                       </div>
                       <div class="relative">
-                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvhmQuVx235_YgiZBbG7BPiA22a7EjlV4qUg&s" alt="Profile" class="rounded-2xl" width="500" height="600">
+                          <img src="/https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvhmQuVx235_YgiZBbG7BPiA22a7EjlV4qUg&s" alt="Profile" class="rounded-2xl" width="500" height="600">
                       </div>
                   </div>
               </div>
